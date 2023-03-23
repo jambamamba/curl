668f2dd8af33913e69bdf358a194cfd9584841c1
oosman <oosman@foo.bar>
2023-03-23 19:49:38 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -xe
 set -xe
 
+source share/pins.txt
 source share/scripts/helper-functions.sh
 
 function skip(){
     local target="x86"
+    local builddir=""
     parseArgs $@
-    local builddir="${target}-build"
     local SHA="$(sudo git config --global --add safe.directory .;sudo git rev-parse --verify --short HEAD)"
     local package="${library}-${SHA}-${target}.tar.xz"
 
@@ -29,27 +30,24 @@ function skip(){
 function build(){
     local clean=""
     local target="x86"
+    local builddir=""
     local cmake_toolchain_file=""
     parseArgs $@
     
-    local builddir="${target}-build"
-    mkdir -p "${builddir}"
-    # local postfix="-1.1.1t"
-
-    if [ "$clean" == "true" ]; then
-        rm -fr "${builddir}/*"
-    fi
-
     local script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     export ZLIB_LIBRARY=${script_dir}
     export ZLIB_INCLUDE_DIR=${script_dir}
-    pushd "${builddir}"
 
+    local srcdir="$(pwd)"
+    local cmake_modules_path="${srcdir}/share/cmake-modules"
+    pushd "${builddir}"
     if [ "$target" == "mingw" ]; then
-        source "../share/toolchains/x86_64-w64-mingw32.sh"
+        source "${srcdir}/share/toolchains/x86_64-w64-mingw32.sh"
         cmake \
             -DCMAKE_INSTALL_BINDIR=$(pwd) \
             -DCMAKE_INSTALL_LIBDIR=$(pwd) \
+            -DCMAKE_MODULE_PATH="${cmake_modules_path}" \
+            -DCMAKE_PREFIX_PATH="${cmake_modules_path}" \
             -DBUILD_SHARED_LIBS=ON \
             -DCMAKE_SKIP_RPATH=TRUE \
             -DCMAKE_SKIP_INSTALL_RPATH=TRUE \
@@ -70,27 +68,56 @@ function build(){
             -DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32 \
             -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
             -DCMAKE_INSTALL_PREFIX=../install-win \
-            -G "Ninja" ..
+            -DMINGW=1 \
+            -DCMAKE_TOOLCHAIN_FILE="${srcdir}/curl-options.cmake" \
+            -G "Ninja" .. 
     elif [ "$target" == "arm" ]; then
         source "${SDK_DIR}/environment-setup-aarch64-fslc-linux"
         cmake \
             -DBUILD_SHARED_LIBS=ON \
+            -DCMAKE_MODULE_PATH="${cmake_modules_path}" \
+            -DCMAKE_PREFIX_PATH="${cmake_modules_path}" \
+            -DCMAKE_TOOLCHAIN_FILE="${srcdir}/curl-options.cmake" \
             -G "Ninja" ..
     elif [ "$target" == "x86" ]; then
 		export STRIP="$(which strip)"
         cmake \
             -DBUILD_SHARED_LIBS=ON \
+            -DCMAKE_TOOLCHAIN_FILE="${srcdir}/curl-options.cmake" \
             -G "Ninja" ..
     fi
-    ninja
+    ninja --verbose
     rsync -uav lib/libcurl* .
     popd
 }
 
+function installDependencies() {
+    local target="x86"
+    local builddir=""
+    parseArgs $@
+    local artifacts_url="/home/$USER/Downloads"
+    local libs=(zlib openssl)
+    for library in "${libs[@]}"; do
+        local pin="${library}_pin"
+        # echo "${!pin}" #gets the value of variable where the variable name is "${library}_pin"
+        local artifacts_file="${library}-${!pin}-${target}.tar.xz"
+        installLib $@ library="${library}" artifacts_file="${artifacts_file}" artifacts_url="${artifacts_url}" 
+    done
+}
+
 function main(){
-   skip $@ library="curl"
-   build $@
-   package $@ library="curl"
+    parseArgs $@
+
+    local builddir="$(pwd)/${target}-build"
+    if [ "$clean" == "true" ]; then
+        rm -fr "${builddir}"
+    fi
+    mkdir -p "${builddir}"
+
+    skip $@ library="curl"
+    installDependencies $@ builddir="${builddir}"
+    build $@ builddir="${builddir}"
+    package $@ library="curl" builddir="${builddir}"
 }
 
 time main $@
